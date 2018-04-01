7660151d64c9efa9d53f42556aeb6c39782af4b4
Jorge Ulises <naturalmentejorge@gmail.com>
2018-04-01 12:26:01 -0500

    Update git-profile.sh

--- git-profile.sh
+++ git-profile.sh
@@ -1,42 +1,62 @@
 #!/bin/bash
 
+function addNew {
+  DEFAULT_PROFILE_NICKNAME="Jorge"
+  echo "Add user nickname: [ $DEFAULT_PROFILE_NICKNAME ]"
+  read PROFILE_NICKNAME
+  if [ "$PROFILE_NICKNAME" = "" ]
+  then
+    PROFILE_NICKNAME="$DEFAULT_PROFILE_NICKNAME"
+  fi
+  
+  DEFAULT_PROFILE_FULLNAME="Jorge Ulises Useche Cuellar"
+  echo "Add user fullname: [ $DEFAULT_PROFILE_FULLNAME ]"
+  read PROFILE_FULLNAME
+  if [ "$PROFILE_FULLNAME" = "" ]
+  then
+    PROFILE_FULLNAME="$DEFAULT_PROFILE_FULLNAME"
+  fi
+    
+  DEFAULT_PROFILE_EMAIL="naturalmentejorge@gmail.com"
+  echo "Add user git email: [ $DEFAULT_PROFILE_EMAIL ]"
+  read PROFILE_EMAIL
+  if [ "$PROFILE_EMAIL" = "" ]
+  then
+    PROFILE_EMAIL="$DEFAULT_PROFILE_EMAIL"
+  fi
+  
+  if [[ ! -e "$fileprofiles" ]]
+  then  
+  tee "$fileprofiles" << EOF
+# put dot comma separated values @see https://github.com/juusechec/git-profile/blob/master/profiles.lst
+$PROFILE_NICKNAME;$PROFILE_FULLNAME;$PROFILE_EMAIL
+EOF
+  else
+  echo "$PROFILE_NICKNAME;$PROFILE_FULLNAME;$PROFILE_EMAIL" >> "$fileprofiles" 
+  fi
+  echo "Modificated $fileprofiles"
+}
+
 fileprofiles="profiles.lst"
-if [ ! -f $fileprofiles ]; then
+if [ ! -f $fileprofiles ]
+then
     fileprofiles="$HOME/profiles.lst"
     if [[ ! -e "$fileprofiles" ]]
     then
     echo "File $fileprofiles not found!"
-    
-    DEFAULT_PROFILE_NICKNAME="Jorge"
-    echo "Add user nickname: [ $DEFAULT_PROFILE_NICKNAME ]"
-    read PROFILE_NICKNAME
-    if [ "$PROFILE_NICKNAME" = "" ]
-    then
-    PROFILE_NICKNAME="$DEFAULT_PROFILE_NICKNAME"
+      addNew
+      exit
     fi
-    
-    DEFAULT_PROFILE_FULLNAME="Jorge Ulises Useche Cuellar"
-    echo "Add user fullname: [ $DEFAULT_PROFILE_FULLNAME ]"
-    read PROFILE_FULLNAME
-    if [ "$PROFILE_FULLNAME" = "" ]
-    then
-    PROFILE_FULLNAME="$DEFAULT_PROFILE_FULLNAME"
-    fi
-    
-    DEFAULT_PROFILE_EMAIL="naturalmentejorge@gmail.com"
-    echo "Add user git email: [ $DEFAULT_PROFILE_EMAIL ]"
-    read PROFILE_EMAIL
-    if [ "$PROFILE_EMAIL" = "" ]
-    then
-    PROFILE_EMAIL="$DEFAULT_PROFILE_EMAIL"
-    fi
-    
-    tee "$fileprofiles" << EOF
-# put dot comma separated values @see https://github.com/juusechec/git-profile/blob/master/profiles.lst
-$PROFILE_NICKNAME;$PROFILE_FULLNAME;$PROFILE_EMAIL
-EOF
-    echo "Modificated $fileprofiles"
-    fi
+fi
+
+if [ "$1" == "" ]
+then
+  echo "You can add a new user to $fileprofiles, Ctrl+C to Cancel"
+  printf "\nCurrent value:\n\n"
+  cat $fileprofiles
+  printf "\nPlease, add data for new git user\n\n"
+  addNew
+  exit
 fi
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
