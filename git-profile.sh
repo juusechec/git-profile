5a4b79eeec03504ea4e13be3e258c6f8e07501f5
Jorge Ulises <naturalmentejorge@gmail.com>
2018-04-01 11:55:13 -0500

    Update git-profile.sh

--- git-profile.sh
+++ git-profile.sh
@@ -1,5 +1,44 @@
 #!/bin/bash
 
+fileprofiles="profiles.lst"
+if [ ! -f $fileprofiles ]; then
+    fileprofiles="$HOME/profiles.lst"
+    if [[ ! -e "$fileprofiles" ]]
+    then
+    echo "File $fileprofiles not found!"
+    
+    $DEFAULT_PROFILE_NICKNAME="Jorge"
+    echo "Add user nickname: [ $DEFAULT_PROFILE_NICKNAME ]"
+    read PROFILE_NICKNAME
+    if [ "$PROFILE_NICKNAME" = "" ]
+    then
+    PROFILE_NICKNAME="$DEFAULT_PROFILE_NICKNAME"
+    fi
+    
+    $DEFAULT_PROFILE_FULLNAME="Jorge Ulises Useche Cuellar"
+    echo "Add user fullname: [ $DEFAULT_PROFILE_FULLNAME ]"
+    read PROFILE_FULLNAME
+    if [ "$PROFILE_FULLNAME" = "" ]
+    then
+    PROFILE_FULLNAME="$DEFAULT_PROFILE_FULLNAME"
+    fi
+    
+    $DEFAULT_PROFILE_EMAIL="naturalmentejorge@gmail.com"
+    echo "Add user git email: [ $DEFAULT_PROFILE_EMAIL ]"
+    read PROFILE_EMAIL
+    if [ "$PROFILE_EMAIL" = "" ]
+    then
+    PROFILE_EMAIL="$DEFAULT_PROFILE_EMAIL"
+    fi
+    
+    tee "$file" << EOF
+# put dot comma separated values @see https://github.com/juusechec/git-profile/blob/master/profiles.lst
+$PROFILE_NICKNAME;$PROFILE_FULLNAME;$PROFILE_EMAIL
+EOF
+    echo "Modificated $fileprofiles."
+    fi
+fi
+
 while IFS='' read -r line || [[ -n "$line" ]]; do
     #echo "Text read from file: $line"
     profile=$(echo $line | tr ";" "\n")
@@ -14,4 +53,4 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
       echo "Your git is ready to commit!"
       break
     fi
-done < "profiles.lst"
+done < "$fileprofiles"
